b2e9f2118a3108f915d4d3826022036131167623
Adedeji1 <afolabiadedeji110@yahoo.com>
2025-11-07 12:53:50 +0100

    correction

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,7 +7,7 @@ interface StudentConstructor {
   new (firstName: string, lastName: string): StudentClassInterface;
 }
 
-class StudentClass implements StudentClassInterface {
+class StudentClass {
   firstName: string;
   lastName: string;
 
